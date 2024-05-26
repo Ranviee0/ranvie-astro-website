9f3a04cffe25a6af909f81ef8f811199a59c6feb
Ranviee0 <neodyte@gmail.com>
2024-05-26 15:47:34 +0700

    Minor Changes

--- src/json/projects.js
+++ src/json/projects.js
@@ -3,7 +3,12 @@ const Projects = [
     image: "webpage_capture.png",
     name: "เว็บไซต์นี้",
     desc: "สิ่งที่คุณกำลังเข้าชมอยู่ในตอนนี้",
-    links: [{ site: "Github", url: "https://www.github.com/" }],
+    links: [
+      {
+        site: "Github",
+        url: "https://github.com/Ranviee0/ranvie-astro-website",
+      },
+    ],
   },
 ];
 
